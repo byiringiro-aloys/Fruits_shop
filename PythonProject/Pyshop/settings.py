2bc3aabb42901f829c9ad43a95f449f62ba57fee
byiringiro-aloys <byiringiroaloys8@gmail.com>
2025-04-14 08:23:50 +0200

    remove sensitive info

--- PythonProject/Pyshop/settings.py
+++ PythonProject/Pyshop/settings.py
@@ -69,15 +69,7 @@ WSGI_APPLICATION = 'Pyshop.wsgi.application'
 
 
 # Database
-
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-'''
+# 
 
 DATABASES = {
 'default' : dj_database_url.parse(os.environ.get('databaseUri'))
@@ -106,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
+#
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
